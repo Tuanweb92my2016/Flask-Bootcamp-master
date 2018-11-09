fa50b7889d726784696093625207f96f5f5bac31
tuanweb <nguyenanhtuan92my@gmail.com>
2018-11-09 12:30:34 +0700

    Revert "forms"
    This reverts commit ada281a3998a5325fb7db5a9b1ce53dc2f63fba4.

--- Flask/basic.py
+++ Flask/basic.py
@@ -1,26 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return "<h1> Hello Puppy ! </h1>"
 
-    return render_template('index.html')
-
-@app.route('/signup_form')
-def signup_form():
-
-    return render_template('signup.html')
-
-@app.route('/thank_you')
-def thank_you():
-    first = request.args.get('first')
-    last  = request.args.get('last')
-    return render_template('thankyou.html',first=first,last=last)
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'),404
+@app.route('/information')
+def info():
+    return "<h1> Puppy are cute !</h1>"
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
