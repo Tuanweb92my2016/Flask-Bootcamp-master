7a81c8b619430a25ca3cdbd257dbeb9f95efaad5
tuanweb <nguyenanhtuan92my@gmail.com>
2018-11-19 04:08:18 +0000

    oauth

--- Oauth/app.py
+++ Oauth/app.py
@@ -1,66 +1,41 @@
 #app.py
 
-from myproject import app,db
-from flask import render_template,redirect,request,url_for,Flask,abort
-from flask_login import login_user,login_required,logout_user
-from myproject.models import User
-from myproject.forms import LoginForm,RegistrationForm
-from werkzeug.security import generate_password_hash,check_password_hash
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+
+###################################
+from flask import Flask,redirect,url_for,render_template
+from flask_dance.contrib.google import make_google_blueprint,google
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+
+blueprint = make_google_blueprint(client_id='1004369005965-vs8eh4i514fp8paouvged6do1tf5d1nm.apps.googleusercontent.com',client_secret='0bxKrGPW36pDU_7HB0WSOZM5',offline=True,scope=['profile','email'])
+
+app.register_blueprint(blueprint,url_prefix='/login')
 
 @app.route('/')
-def home():
+def index():
     return render_template('home.html')
 
 @app.route('/welcome')
-@login_required
-def welcome_user():
-    return render_template('welcome_user.html')
+def welcome():
+    # return error internal server error if not logged in !
+    resp = google.get('/oauth2/v2/userinfo')
+    assert resp.ok,resp.text
+    email = resp.json()['email']
+    return render_template('welcome.html',email=email)
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flask("You logged out!")
-    return redirect(url_for('home'))
-
-
-
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login/google')
 def login():
+    if not google.authorized:
+        return render_template('google.login')
+    resp = google.get('/oauth2/v2/userinfo')
+    assert resp.ok,resp.text
+    email = resp.json()['email']
 
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-
-        if user.check_password(form.password.data) and user is not None :
-
-            login_user(user)
-            flask('Logged in Successfully!')
-
-            next = request.args.get('next')
-
-            if next == None or not next[0]=='/':
-                next = url_for('welcome_user')
-
-            return redirect(next)
-
-        return render_template('login.html',form=form)
-
-@app.route('/register',methods=['GET','POST'])
-def register():
-    form = RegistrationForm()
-
-    if form.validate_on_submit():
-        user = User(email=form.email.data,
-            username=form.username.data,
-            password=form.password.data)
-
-        db.session.add(user)
-        db.session.commit()
-        flask("Thanks for registeration!")
-        return redirect(url_for('login'))
-
-    return render_template('register.html',form=form)
+    return render_template('welcome.html',email=email)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
