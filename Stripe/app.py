415640e499a8637e9101d0d7b6ddb6d5b7a4db32
tuanweb <nguyenanhtuan92my@gmail.com>
2018-11-22 09:53:51 +0000

    complated 2

--- Stripe/app.py
+++ Stripe/app.py
@@ -1,34 +1,27 @@
 import os
-from flask import Flask, render_template, request,redirect,url_for
+from flask import Flask, render_template, request
 import stripe
+
+stripe_keys = {
+  'secret_key': os.environ['SECRET_KEY'],
+  'publishable_key': os.environ['PUBLISHABLE_KEY']
+}
+
+stripe.api_key = stripe_keys['secret_key']
 
 app = Flask(__name__)
 
-public_key = "pk_test_FzniVv5eiHRWAknHDpYK9vHm"
-
-stripe_keys = "sk_test_PVahTzNmDwc6lBwmwM7Qp7Sg"
-
-# {'secret_key': os.environ['SECRET_KEY'],'publishable_key': os.environ['PUBLISHABLE_KEY']}
-
-# stripe.api_key = stripe_keys['secret_key']
-
-
-
 @app.route('/')
 def index():
-    return render_template('index.html',public_key=public_key)
+    return render_template('index.html', key=stripe_keys['publishable_key'])
 
-@app.route('/thankyou')
-def thankyou():
-    return render_template('thankyou.html')
-
-@app.route('/payment', methods=['POST'])
-def payment():
+@app.route('/charge', methods=['POST'])
+def charge():
     # Amount in cents
     amount = 500
 
     customer = stripe.Customer.create(
-        email= request.form['stripeEmail'],
+        email='customer@example.com',
         source=request.form['stripeToken']
     )
 
@@ -39,8 +32,7 @@ def payment():
         description='Flask Charge'
     )
 
-    # return render_template('charge.html', amount=amount)
-    return redirect(url_for('thankyou'))
+    return render_template('charge.html', amount=amount)
 
 if __name__ == '__main__':
     app.run(debug=True)
